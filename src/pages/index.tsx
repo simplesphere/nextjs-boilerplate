e97eac7a30621ec878898816f8347539cdb34bb8
simplesphere <anoop.jeewoolall@gmail.com>
2023-01-14 20:37:11 -0500

    build: added prettier + configured + added prettier commands

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
 export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>NextSimple - NextJs Boilerplate to scaffold apps quickly</title>
-				<meta name="description" content="NextJs Boilerplate built for developers" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
+				<meta name='description' content='NextJs Boilerplate built for developers' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className="bg-grey-50">
-				<div className="container flex flex-wrap items-center">
-					<h1 className="text-4xl">Hello World</h1>
+			<main className='bg-grey-50'>
+				<div className='container flex flex-wrap items-center'>
+					<h1 className='text-4xl'>Hello World</h1>
 				</div>
 			</main>
 		</>
-	);
+	)
 }
